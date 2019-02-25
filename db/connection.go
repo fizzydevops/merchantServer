9cc8f1977803bba25cdb3de0bbd24ed69756458f
Ryan Claude Fox <rfox@plugnpay.com>
2019-02-25 17:05:22 -0500

    goodies

--- db/connection.go
+++ db/connection.go
@@ -11,8 +11,10 @@ import (
 
 type db struct {
 	conn *sql.DB
+	database string
 }
 
+// NewConnection establishes a new connection with the database provided.
 func NewConnection(database string) *db {
 	credentials, err := getDatabaseCredentials()
 
@@ -21,13 +23,20 @@ func NewConnection(database string) *db {
 	}
 
 	// Try to connect to database with current credentials.
-	conn, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", credentials["/db/merchantdb/sql/endpoint"], credentials["db/merchantdb/sql/password"], database))
+	conn, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", credentials["/db/merchantdb/sql/endpoint"], credentials["/db/merchantdb/sql/password"], database))
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	return &db{conn: conn}
+	// Ping to make sure we establish a connection
+	err = conn.Ping()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return &db{conn: conn, database: database}
 }
 
 // getDatabaseCredentials retrieves the endpoint and password from aws parameter store.
@@ -54,8 +63,61 @@ func getDatabaseCredentials() (map[string]string, error) {
 			results[*value.Name] = *value.Value
 		}
 	} else {
-		// Throw some error because nothing came back.
+		err = &noCredentialsFoundError{err: "No credentials found.", database: "merchantdb"}
 	}
 
-	return results, nil
+	return results, err
+}
+
+// PrepareAndExecute a method to be used for INSERTS, UPDATES, and DELETES
+func (db *db) PrepareAndExecute(query string, values[]interface{}) (sql.Result,error) {
+	stmt, err := db.conn.Prepare(query)
+	defer stmt.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := stmt.Exec(values...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+// QueryAndScan is a method to do a select query and return it as a map.
+func (db *db) QueryAndScan(query string, values[]interface{}) (map[string]interface{}, error) {
+	rows, err := db.conn.Query(query, values...)
+	defer rows.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	cols, _ := rows.Columns()
+	results := make(map[string]interface{})
+
+	for rows.Next() {
+		columns := make([]interface{}, len(cols))
+		columnPointers := make([]interface{},len(cols))
+
+		for i := range columns {
+			columnPointers[i] = &columns[i]
+		}
+
+		err := rows.Scan(columnPointers...)
+
+		if err != nil {
+			return nil, err
+		}
+
+		for i, colName := range cols {
+			value := columnPointers[i].(*interface{})
+			results[colName] = *value
+		}
+	}
+
+	return results, err
 }
---
+++ db/error.go
@@ -0,0 +1,12 @@
+package db
+
+import "fmt"
+
+type noCredentialsFoundError struct {
+	err string
+	database string
+}
+
+func (e *noCredentialsFoundError) Error() string {
+	return fmt.Sprintf(e.err + "\n database: %s", e.database)
+}
